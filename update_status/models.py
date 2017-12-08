4c133579a8778b6f179ac37e8a5caee942063e36
JosuaChristanto <jossi_christ@ymail.com>
2017-12-08 22:23:37 +0700

    halaman_status

--- update_status/models.py
+++ update_status/models.py
@@ -1,22 +1,3 @@
-from __future__ import unicode_literals
-
 from django.db import models
-from django.utils import timezone
-import pytz
 
 # Create your models here.
-class Pengguna(models.Model):
-    kode_identitas = models.CharField('Kode Identitas', max_length=20, primary_key=True, )
-    nama = models.CharField('Nama', max_length=200)
-    created_at = models.DateTimeField(default=timezone.now() + timezone.timedelta(hours=7))
-    updated_at = models.DateTimeField(default=timezone.now() + timezone.timedelta(hours=7))
-
-class Status(models.Model):
-    pengguna = models.ForeignKey(Pengguna)
-    status = models.CharField(max_length=1000)
-    created_date = models.DateTimeField(default=timezone.now() + timezone.timedelta(hours=7))
-
-class Comment(models.Model):
-	pengguna = models.ForeignKey(Status,on_delete=models.CASCADE)
-	comment = models.CharField(max_length=1000)
-	created_date = models.DateTimeField(default=timezone.now() + timezone.timedelta(hours=7))
