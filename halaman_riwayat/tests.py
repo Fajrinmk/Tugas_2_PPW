2291cefd816777d0a00a632a3a51fe948bc5172f
Safira Amalia <safira.amalia@gmail.com>
2017-12-11 23:40:45 +0700

    repair get riwayat

--- halaman_riwayat/tests.py
+++ halaman_riwayat/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
 
 # Create your tests here.
+# class RiwayatUnitTest(TestCase):
+#     def test_riwayat_url_is_exist(self):
+#         response = Client().get('/riwayat/')
+#         self.assertEqual(response.status_code, 200)
--- halaman_riwayat/views.py
+++ halaman_riwayat/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
-from django.urls import reverse
 from django.http import HttpResponseRedirect
-from hal_riwayat.js import database_mahasiswa
+from django.urls import reverse
+import requests
 
 # Create your views here.
-#index 
-#mengambil data di js ngambil,parameter npm
-
 response = {}
+RIWAYAT_API = 'https://private-e52a5-ppw2017.apiary-mock.com/riwayat'
 
 def index(request):
-    if 'user_login' in request.session:
-        return HttpResponseRedirect(reverse('halaman_riwayat : riwayat'))
-    else:
-        response['login'] = False
-        html = 'lab_9/session/login.html'
-        return render(request, html, response)
+	response['daftar_riwayat'] = get_riwayat()
+	html = 'halaman_riwayat/riwayat.html'
+	return render(request, html, response)
 
-# def get_data_kelas(access_token, id):
-#     parameters = {"access_token": access_token, "client_id": get_client_id()}
-#     response = requests.get(API_MAHASISWA+id, params=parameters)
-#     return response.json()
+def get_riwayat():
+	riwayat = requests.get(RIWAYAT_API)
+	return riwayat.json()
