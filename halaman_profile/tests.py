3c718d8ed7e5d3055895f50e2ed1778e0483837c
fajrinmk <fajrinmk@gmail.com>
2017-12-12 20:04:19 +0700

    tdd nanti dulu ya, dicomment dulu

--- halaman_profile/tests.py
+++ halaman_profile/tests.py
@@ -38,14 +38,14 @@
 #         self.assertTrue(len(expert) >= 3)
 
 
-from django.test import TestCase
-from django.test import Client
+# from django.test import TestCase
+# from django.test import Client
 
-# Create your tests here.
-class PageRiwayatUnitTest(TestCase):
-    def test_page_profile_url_is_exist(self):
-    	response = Client().get('/page_profile/')
-    	self.assertEqual(response.status_code, 200)
+# # Create your tests here.
+# class PageRiwayatUnitTest(TestCase):
+#     def test_page_profile_url_is_exist(self):
+#     	response = Client().get('/page_profile/')
+#     	self.assertEqual(response.status_code, 200)
 
 
     
